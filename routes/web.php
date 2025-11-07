d469930318916a4971754203d7dc471db3169505
Zia Uddin Robin <74249433+nzian@users.noreply.github.com>
2025-11-07 11:29:42 +0600

    login expire, changes user password and other

--- routes/web.php
+++ routes/web.php
@@ -529,7 +529,7 @@ Route::prefix('care-continuity')->name('care-continuity.')->group(function () {
     Route::get('/', [CareContinuityController::class, 'index'])->name('index');
     
     // Follow-up Appointments
-    Route::post('/appointments/store', [CareContinuityController::class, 'storeFollowUpAppointment'])->name('appointments.store');
+    Route::post('/appointments/store', [CareContinuityController::class, 'storeFollowUpAppointment'])->name('care.appointments.store');
     Route::put('/appointments/{id}', [CareContinuityController::class, 'updateFollowUpAppointment'])->name('appointments.update');
     Route::delete('/appointments/{id}', [CareContinuityController::class, 'destroyFollowUpAppointment'])->name('appointments.destroy');
     
